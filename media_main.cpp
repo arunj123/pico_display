c5cf4972ac0d391775e72119e76409b494f38cdf
Arun J <arunj123@gmail.com>
2025-09-21 19:04:02 +0200

    Fix keypress sending.

--- media_main.cpp
+++ media_main.cpp
@@ -3,56 +3,64 @@
 #include "MediaControllerDevice.h"
 #include "BtStackManager.h"
 #include "btstack_run_loop.h"
-#include "RotaryEncoder.h" // Include our new encoder class
+#include "RotaryEncoder.h"
 #include <cstdio>
 
 // --- Define hardware pins and polling rate ---
 constexpr uint ENCODER_PIN_A = 10;
-constexpr int POLLING_INTERVAL_MS = 100;
+constexpr int POLLING_INTERVAL_MS = 75;  // Poll a little faster for smoother response
 constexpr int RELEASE_DELAY_MS = 50;
 
 // --- Application Objects ---
 static MediaControllerDevice mediaController;
-static RotaryEncoder encoder(pio1, ENCODER_PIN_A); // Use the free pio1
+static RotaryEncoder encoder(pio1, ENCODER_PIN_A); 
 
-// --- Timers ---
+// --- Timers and State ---
 static btstack_timer_source_t release_timer;
 static btstack_timer_source_t polling_timer;
-static int last_rotation = 0;
+static int processed_rotation = 0; // Keeps track of the value we've sent commands for
 
-// This function will be called by the timer to send the "key up" command
+// This function is called by the timer to send the "key up" command
 static void release_handler(btstack_timer_source_t *ts){
     (void)ts; // Unused parameter
-    mediaController.release();
+    if (mediaController.isConnected()) {
+        mediaController.release();
+    }
 }
 
 // This is the main polling handler for the rotary encoder
 static void polling_handler(btstack_timer_source_t *ts) {
-    int current_rotation = encoder.get_rotation();
-    int delta = current_rotation - last_rotation;
+    // Only process if we are connected to a host
+    if (mediaController.isConnected()) {
+        int current_rotation = encoder.get_rotation();
 
-    if (delta > 0) { // Clockwise rotation
-        printf("Volume Up\n");
-        mediaController.increaseVolume();
-        // After sending volume up, schedule a release
-        btstack_run_loop_set_timer(&release_timer, RELEASE_DELAY_MS);
-        btstack_run_loop_add_timer(&release_timer);
-    } else if (delta < 0) { // Counter-clockwise rotation
-        printf("Volume Down\n");
-        mediaController.decreaseVolume();
-        // After sending volume down, schedule a release
-        btstack_run_loop_set_timer(&release_timer, RELEASE_DELAY_MS);
-        btstack_run_loop_add_timer(&release_timer);
+        // Check if we need to send a "Volume Up" command
+        if (current_rotation > processed_rotation) {
+            printf("Volume Up (Target: %d, Processed: %d)\n", current_rotation, processed_rotation);
+            mediaController.increaseVolume();
+            btstack_run_loop_set_timer(&release_timer, RELEASE_DELAY_MS);
+            btstack_run_loop_add_timer(&release_timer);
+            processed_rotation++; // We have now processed this single step up
+        } 
+        // Check if we need to send a "Volume Down" command
+        else if (current_rotation < processed_rotation) {
+            printf("Volume Down (Target: %d, Processed: %d)\n", current_rotation, processed_rotation);
+            mediaController.decreaseVolume();
+            btstack_run_loop_set_timer(&release_timer, RELEASE_DELAY_MS);
+            btstack_run_loop_add_timer(&release_timer);
+            processed_rotation--; // We have now processed this single step down
+        }
     }
 
-    last_rotation = current_rotation;
-
-    // Reset the polling timer for the next check
+    // Always reset the polling timer for the next check
     btstack_run_loop_set_timer(ts, POLLING_INTERVAL_MS);
     btstack_run_loop_add_timer(ts);
 }
 
 extern "C" int btstack_main(void) {
+    // Initialize the processed rotation to the encoder's starting value
+    processed_rotation = encoder.get_rotation();
+
     // Set up the auto-release timer
     btstack_run_loop_set_timer_handler(&release_timer, release_handler);
     
