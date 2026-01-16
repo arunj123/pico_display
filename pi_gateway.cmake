a05c69844328fba0691762668bd22245e572fc8a
Arun J <arunj123@gmail.com>
2026-01-16 20:35:08 +0100

    Cleanup cmake

--- pi_gateway.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.13)
-
-add_custom_target(pi_image
-    COMMAND ${CMAKE_SOURCE_DIR}/scripts/build_pi_image.sh
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMENT "Building Raspberry Pi Zero Image with Buildroot..."
-    USES_TERMINAL
-)
-
